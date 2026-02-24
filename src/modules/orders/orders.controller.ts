8bf9fc3d2369eebf69d0ce9c145799c2a423962b
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2026-02-24 10:57:22 +0600

    orders-api-done

--- src/modules/orders/orders.controller.ts
+++ src/modules/orders/orders.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 
+import { CreateOrderDto } from './dto/create-order.dto';
+import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth-guards';
+import { GetUser } from 'src/common/decorators/get-user-decorators';
+import { OrderService } from './orders.service';
+
+@ApiTags('orders')
 @Controller('orders')
-export class OrdersController {}
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth('JWT-auth')
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Place Order (Checkout)' })
+  createOrder(@GetUser('id') userId: string, @Body() dto: CreateOrderDto) {
+    return this.orderService.createOrder(userId, dto);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Get my order history' })
+  getMyOrders(@GetUser('id') userId: string) {
+    return this.orderService.getOrders(userId);
+  }
+}
